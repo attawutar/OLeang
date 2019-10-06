7bb0f93484cd6ef97da6b8136a99b23643f58fe6
Attawut Arpornrat <60070116@kmitl.ac.th>
2019-10-07 03:04:22 +0700

    ตีสามแล้วไอสัสง่วง

---
+++ javascript.js
@@ -0,0 +1,13 @@
+function carDrive(){
+  var elem = document.getElementsByClassName("car");   
+  var pos = 40;
+  var id = setInterval(frame, 10);
+  function frame() {
+    if (pos == 100) {
+      clearInterval(id);
+    } else {
+      pos++; 
+      elem.style.marginLeft = pos + "%"; 
+    }
+  }
+  }
